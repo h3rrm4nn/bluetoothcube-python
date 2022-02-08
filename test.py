14221df61bdb06f9d57a25a2ec7738d02074b657
Matthias Herrmann <herrmann@eit.uni-kl.de>
2022-02-08 23:34:25 +0100

    Moved bluetooth functions to bluetooth cube class

--- test.py
+++ test.py
@@ -8,15 +8,6 @@ from bleak import BleakClient
 from Crypto.Cipher import AES
 
 
-# class Cube:
-
-#     def __init__(self):
-#         self.state = name
-
-#     def move(self, move):
-#         if move == "U":
-#             self.
-
 # Top   | Bottom
 
 # 2   3 | 6   7
@@ -24,12 +15,12 @@ from Crypto.Cipher import AES
 # 1   0 | 5   4
 #   G   |   G
 #
-# Top   | Bottom | Middle
+# Top   | Middle  | Bottom
 
-#   3   |   7    | 10   11
-# 2 W 0 | 6 W 4  |    W
-#   1   |   5    |  9    8
-#   G   |   G    |    G
+#   3   | 10   11 |   7
+# 2 W 0 |    W    | 6 W 4
+#   1   |  9    8 |   5
+#   G   |    G    |   G
 #
 
 class Cube:
@@ -52,14 +43,15 @@ class Cube:
             edge_perm = []
             twist_corners = False
             twist_edges = False
+            # TODO:
             if (move[i] == "U" or move[i] == "Up"):
-                corner_perm   = [ 0, 1, 2, 3]
-                edge_perm     = [ 0, 1, 2, 3]
+                corner_perm   = [ 1, 2, 3, 0]
+                edge_perm     = [ 1, 2, 3, 0]
                 twist_corners = False
                 twist_edges   = False
             elif (move[i] == "D" or move[i] == "Dp"):
-                corner_perm   = [ 4, 7, 6, 5]
-                edge_perm     = [ 4, 7, 6, 5]
+                corner_perm   = [ 7, 6, 5, 4]
+                edge_perm     = [ 7, 6, 5, 4]
                 twist_corners = False
                 twist_edges   = False
             elif (move[i] == "R" or move[i] == "Rp"):
@@ -68,8 +60,8 @@ class Cube:
                 twist_corners = True
                 twist_edges   = False
             elif (move[i] == "L" or move[i] == "Lp"):
-                corner_perm   = [ 1, 5, 6, 2]
-                edge_perm     = [ 2, 9, 6,10]
+                corner_perm   = [ 5, 6, 2, 1]
+                edge_perm     = [ 9, 6,10, 2]
                 twist_corners = True
                 twist_edges   = False
             elif (move[i] == "F" or move[i] == "Fp"):
@@ -78,8 +70,8 @@ class Cube:
                 twist_corners = True
                 twist_edges   = True
             elif (move[i] == "B" or move[i] == "Bp"):
-                corner_perm   = [ 2, 6, 7, 3]
-                edge_perm     = [10, 7,11, 3]
+                corner_perm   = [6, 7, 3, 2]
+                edge_perm     = [7,11, 3,10]
                 twist_corners = True
                 twist_edges   = True
 
@@ -135,196 +127,182 @@ class Cube:
         print(self.edge_twist)
 
 
+class Bluetooth_cube:
 
-address = "AB:12:34:01:42:00"
-device_key = [0x00, 0x42, 0x01, 0x34, 0x12, 0xab]
+    def __init__(self):
 
-GAN_V2_KEY = [
-    0x01, 0x02, 0x42, 0x28, 0x31, 0x91, 0x16, 0x07, 0x20, 0x05, 0x18, 0x54, 0x42, 0x11,
-    0x12, 0x53,
-]
-GAN_V2_IV = [
-    0x11, 0x03, 0x32, 0x28, 0x21, 0x01, 0x76, 0x27, 0x20, 0x95, 0x78, 0x14, 0x32, 0x12,
-    0x02, 0x43,
-]
-UUID_LISTEN = "28be4cb6-cd67-11e9-a32f-2a2ae2dbcce4"
-UUID_WRITE = "28be4a4a-cd67-11e9-a32f-2a2ae2dbcce4"
+        # Device dependent key
+        self.address = "AB:12:34:01:42:00"
+        self.device_key = [0x00, 0x42, 0x01, 0x34, 0x12, 0xab]
 
-key = GAN_V2_KEY;
-iv = GAN_V2_IV;
+        # Service UUIDs for GAN bluetooth cubes version 2
+        self.UUID_LISTEN = "28be4cb6-cd67-11e9-a32f-2a2ae2dbcce4"
+        self.UUID_WRITE = "28be4a4a-cd67-11e9-a32f-2a2ae2dbcce4"
 
-for idx, byte in enumerate(device_key) :
-    key[idx] = ((key[idx] + byte) % 255)
-    iv[idx] = ((iv[idx] + byte) % 255)
+        # Key for GAN bluetooth cubes version 2
+        GAN_V2_KEY = [
+            0x01, 0x02, 0x42, 0x28, 0x31, 0x91, 0x16, 0x07, 0x20, 0x05, 0x18, 0x54, 0x42, 0x11,
+            0x12, 0x53,
+        ]
+        GAN_V2_IV = [
+            0x11, 0x03, 0x32, 0x28, 0x21, 0x01, 0x76, 0x27, 0x20, 0x95, 0x78, 0x14, 0x32, 0x12,
+            0x02, 0x43,
+        ]
 
-MOVES = [ "Up", "U", "Rp", "R", "Fp", "F", "Dp", "D", "Lp", "L", "Bp", "B" ];
+        # Compute device key
+        self.key = GAN_V2_KEY;
+        self.iv = GAN_V2_IV;
+        for idx, byte in enumerate(self.device_key) :
+            self.key[idx] = ((self.key[idx] + byte) % 255)
+            self.iv[idx] = ((self.iv[idx] + byte) % 255)
 
-MSG_CUBE_STATE = [4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-MSG_BATTERY_STATE = [9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        # Move mapping for GAN cube
+        self.MOVES = [ "Up", "U", "Rp", "R", "Fp", "F", "Dp", "D", "Lp", "L", "Bp", "B" ];
 
-cube = Cube([0,1,2,3,4,5,6,7],[0,0,0,0,0,0,0,0],[0,1,2,3,4,5,6,7,8,9,10,11],[0,0,0,0,0,0,0,0,0,0,0,0])
+        # Write message to initialize cube state reply
+        self.MSG_CUBE_STATE = [4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+        # Write message to initialize battery state reply
+        self.MSG_BATTERY_STATE = [9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+        # Init virtual cube
+        self.cube = Cube([0,1,2,3,4,5,6,7],[0,0,0,0,0,0,0,0],[0,1,2,3,4,5,6,7,8,9,10,11],[0,0,0,0,0,0,0,0,0,0,0,0])
+
+        self.move_count = 0;
+        self.cube_initialized = False;
 
 
+    def decrypt(self, value):
 
-def decrypt(value):
+        # decrypt last 16 bytes
+        offset = len(value) - 16;
+        end_plain = value[offset :]
 
-    # decrypt last 16 bytes
-    offset = len(value) - 16;
-    end_plain = value[offset :]
+        cipher = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+        end_plain = cipher.decrypt(bytes(end_plain))
+        for idx,byte in enumerate(end_plain) :
+            value[offset+idx] = byte
 
-    cipher = AES.new(bytes(key), AES.MODE_CBC, bytes(iv))
-    end_plain = cipher.decrypt(bytes(end_plain))
-    for idx,byte in enumerate(end_plain) :
-        value[offset+idx] = byte
+        # decrypt first 16 bytes
+        start_plain = value[: 16]
 
-    # decrypt first 16 bytes
-    start_plain = value[: 16]
+        cipher = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+        # cipher.iv = bytes(iv)
+        start_plain = cipher.decrypt(bytes(start_plain))
+        for idx,byte in enumerate(start_plain) :
+            value[idx] = byte
 
-    cipher = AES.new(bytes(key), AES.MODE_CBC, bytes(iv))
-    # cipher.iv = bytes(iv)
-    start_plain = cipher.decrypt(bytes(start_plain))
-    for idx,byte in enumerate(start_plain) :
-        value[idx] = byte
+        return value
 
-    return value
+    def encrypt(self, value):
 
-def encrypt(value):
+        # encrypt first 16 bytes
+        start_plain = value[: 16]
 
-    # encrypt first 16 bytes
-    start_plain = value[: 16]
+        cipher = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+        start_plain = cipher.encrypt(bytes(start_plain))
+        for idx,byte in enumerate(start_plain) :
+            value[idx] = byte
 
-    cipher = AES.new(bytes(key), AES.MODE_CBC, bytes(iv))
-    start_plain = cipher.encrypt(bytes(start_plain))
-    for idx,byte in enumerate(start_plain) :
-        value[idx] = byte
+        # encrypt last 16 bytes
+        offset = len(value) - 16;
+        end_plain = value[offset :]
 
-    # encrypt last 16 bytes
-    offset = len(value) - 16;
-    end_plain = value[offset :]
+        cipher = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+        end_plain = cipher.encrypt(bytes(end_plain))
+        for idx,byte in enumerate(end_plain) :
+            value[offset+idx] = byte
 
-    cipher = AES.new(bytes(key), AES.MODE_CBC, bytes(iv))
-    end_plain = cipher.encrypt(bytes(end_plain))
-    for idx,byte in enumerate(end_plain) :
-        value[offset+idx] = byte
+        return value
 
-    return value
+    def extract_bits(self, data, start, count):
+        result = 0
+        for i in range(count):
+            bit = start + i
+            result = result << 1
+            if data[bit // 8] & (1 << (7 - (bit % 8))) != 0:
+                result |= 1;
+        return result
 
-def extract_bits(data, start, count):
-    result = 0
-    for i in range(count):
-        bit = start + i
-        result = result << 1
-        if data[bit // 8] & (1 << (7 - (bit % 8))) != 0:
-            result |= 1;
-    return result
+    def notification_handler(self, sender, data):
+        data = self.decrypt(data)
+        message_type = self.extract_bits(data, 0, 4);
+        if (message_type == 2) :
+            if (self.cube_initialized):
+                current_move_count = self.extract_bits(data, 4, 8)
+                move_count = current_move_count - self.move_count
+                self.move_count = current_move_count
+                for j in range(move_count):
+                    i = (move_count - 1) - j
+                    move_num = self.extract_bits(data, 12 + i * 5, 5)
+                    # move_time = self.extract_bits(data, 12 + 7 * 5 + i * 16, 16)
+                    move = self.MOVES[move_num]
+                    self.cube.move([move])
+                    self.cube.print_state()
+        elif (message_type == 4) :
+            self.move_count = self.extract_bits(data, 4, 8)
+            self.cube_initialized = True
+            [corner_pos, corner_twist] = self.decode_corners(data)
+            [edge_pos, edge_twist] = self.decode_edges(data)
+            self.cube.set_state(corner_pos, corner_twist, edge_pos, edge_twist)
+            self.cube.print_state()
+        elif (message_type == 9) :
+            self.decode_battery_state(data)
 
-def decode_move(value):
+    def decode_battery_state(self, value):
+        percent = self.extract_bits(value, 8, 8)
+        print("Battery: ", percent, "%")
 
-    message_type = extract_bits(value, 0, 4)
-    current_move_count = extract_bits(value, 4, 8)
+    def decode_corners(self, value):
+        # Decode corners. There are only 7 in the packet because the
+        # last one is implicit (the one missing).
+        corners = [0] * 8
+        corner_twist = [0] * 8
+        total_corner_twist = 0
+        corners_left = list(range(8))
+        for i in range(7):
+            corners[i] = self.extract_bits(value, 12 + i * 3, 3)
+            corner_twist[i] = self.extract_bits(value, 33 + i * 2, 2)
+            total_corner_twist += corner_twist[i]
+            corners_left.remove(corners[i])
+        corners[7] = corners_left[0]
+        corner_twist[7] = (3 - total_corner_twist % 3) % 3
+        return corners, corner_twist
 
-    # assume only one move has been performed
-    i = 1;
-    move_num = extract_bits(value, 12 + i * 5, 5)
-    move_time = extract_bits(value, 12 + 7 * 5 + i * 16, 16)
-    return MOVES[move_num]
+    def decode_edges(self, value):
+        # Decode edges. There are only 11 in the packet because the
+        # last one is implicit (the one missing).
+        edges = [0] * 12
+        edge_parity = [0] * 12
+        total_edge_parity = 0
+        edges_left = list(range(12))
+        for i in range(11):
+            edges[i] = self.extract_bits(value, 47 + i * 4, 4)
+            edge_parity[i] = self.extract_bits(value, 91 + i, 1)
+            total_edge_parity += edge_parity[i]
+            edges_left.remove(edges[i])
+        edges[11] = edges_left[0]
+        edge_parity[11] = total_edge_parity & 1
+        return edges, edge_parity
 
-def notification_handler(sender, data):
-    data = decrypt(data)
-    message_type = extract_bits(data, 0, 4);
-    if (message_type == 2) :
-        # move_count = extract_bits(data, 4, 8)
-        move_count = 1
-        for j in range(move_count):
-            i = (move_count - 1) - j
-            move_num = extract_bits(data, 12 + i * 5, 5)
-            # move_time = extract_bits(data, 12 + 7 * 5 + i * 16, 16)
-            # print(move_num)
-            move = MOVES[move_num]
-            # print(move)
-            cube.move([move])
-            cube.print_state()
-    elif (message_type == 4) :
-        [corner_pos, corner_twist] = decode_corners(data)
-        [edge_pos, edge_twist] = decode_edges(data)
-        cube.set_state(corner_pos, corner_twist, edge_pos, edge_twist)
-        cube.print_state()
-    elif (message_type == 9) :
-        decode_battery_state(data)
+async def run(bt_cube, debug=False):
 
-    # print(', '.join('{:02x}'.format(x) for x in data))
-
-def decode_battery_state(value):
-    percent = extract_bits(value, 8, 8)
-    print("Battery: ", percent, "%")
-
-def decode_corners(value):
-    # Decode corners. There are only 7 in the packet because the
-    # last one is implicit (the one missing).
-    corners = [0] * 8
-    corner_twist = [0] * 8
-    total_corner_twist = 0
-    corners_left = list(range(8))
-    for i in range(7):
-        corners[i] = extract_bits(value, 12 + i * 3, 3)
-        corner_twist[i] = extract_bits(value, 33 + i * 2, 2)
-        total_corner_twist += corner_twist[i]
-        corners_left.remove(corners[i])
-    corners[7] = corners_left[0]
-    corner_twist[7] = (3 - total_corner_twist % 3) % 3
-    # print(corners)
-    # print(corner_twist)
-    return corners, corner_twist
-
-def decode_edges(value):
-    # Decode edges. There are only 11 in the packet because the
-    # last one is implicit (the one missing).
-    edges = [0] * 12
-    edge_parity = [0] * 12
-    total_edge_parity = 0
-    edges_left = list(range(12))
-    for i in range(11):
-        edges[i] = extract_bits(value, 47 + i * 4, 4)
-        edge_parity[i] = extract_bits(value, 91 + i, 1)
-        total_edge_parity += edge_parity[i]
-        edges_left.remove(edges[i])
-    edges[11] = edges_left[0]
-    edge_parity[11] = total_edge_parity & 1
-    return edges, edge_parity
-    # print(edges)
-    # print(edge_parity)
-
-async def run(address, debug=False):
-    log = logging.getLogger(__name__)
-    if debug:
-        import sys
-
-        log.setLevel(logging.DEBUG)
-        h = logging.StreamHandler(sys.stdout)
-        h.setLevel(logging.DEBUG)
-        log.addHandler(h)
-
-    async with BleakClient(address) as client:
+    async with BleakClient(bt_cube.address) as client:
         x = await client.is_connected()
-        log.info("Connected: {0}".format(x))
 
-        await client.start_notify(UUID_LISTEN, notification_handler)
+        await client.start_notify(bt_cube.UUID_LISTEN, bt_cube.notification_handler)
 
         # get initial cube state
-        await client.write_gatt_char(UUID_WRITE, encrypt(MSG_CUBE_STATE))
-        await client.write_gatt_char(UUID_WRITE, encrypt(MSG_BATTERY_STATE))
-        await asyncio.sleep(8.0)
+        await client.write_gatt_char(bt_cube.UUID_WRITE, bt_cube.encrypt(bt_cube.MSG_CUBE_STATE))
+        await client.write_gatt_char(bt_cube.UUID_WRITE, bt_cube.encrypt(bt_cube.MSG_BATTERY_STATE))
+        await asyncio.sleep(20.0)
 
-        await client.stop_notify(UUID_LISTEN)
-
+        await client.stop_notify(bt_cube.UUID_LISTEN)
 
 if __name__ == "__main__":
 
     #Run notify event
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-    # A.move(["L", "U", "Lp", "Up", "B", "R", "Up", "D"])
-    # A.print_state()
-    # A.move(["Up"])
-    # A.print_state()
-    loop.run_until_complete(run(address, True))
+    bt_cube = Bluetooth_cube()
+    loop.run_until_complete(run(bt_cube, True))
